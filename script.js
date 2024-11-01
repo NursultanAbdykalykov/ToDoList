3b884145c52c1a34adb2749384fe0ca85f07df80
Nursultan Abdykalykov <znslt24@gmail.com>
2024-11-01 18:47:58 +0300

    add deleteButton

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buttonAdd = document.querySelector(".add-todo");
 const containerUL = document.querySelector(".todo-list");
 
 
-function createInput(liElement, spanElement){
+function createInput(liElement, spanElement) {
     const inputEdit = document.createElement("input");
     inputEdit.style.width = "25%";
     inputEdit.value = spanElement.textContent;
@@ -13,14 +13,15 @@ function createInput(liElement, spanElement){
         liElement.textContent = "";
         const span = document.createElement("span");
         span.textContent = event.target.value;
-        const editButton = createEditButton(liElement,span);
-        liElement.append(span,editButton);
+        const editButton = createEditButton(liElement, span);
+        const delButton = createDeleteButton(liElement, spanElement);
+        liElement.append(span, editButton, delButton);
 
     });
     return inputEdit;
 }
 
-function createEditButton(liElement, spanElement){
+function createEditButton(liElement, spanElement) {
     const editButton = document.createElement("button");
     editButton.textContent = "Редактировать";
     editButton.addEventListener("click", () => {
@@ -38,12 +39,22 @@ buttonAdd.addEventListener("click", () => {
         let li = document.createElement('li');
         let span = document.createElement("span");
         span.textContent = inputBox.value;
-        const editButton = createEditButton(li,span);
+        const editButton = createEditButton(li, span);
+        const delButton = createDeleteButton(li, span);
         containerUL.appendChild(li);
         li.appendChild(span);
-        li.appendChild(editButton);
+        li.append(editButton, delButton);
     }
     inputBox.value = '';
 });
 
+function createDeleteButton(liElement, spanElement) {
+    const delButton = document.createElement("button");
+    delButton.textContent = "Удалить";
+    delButton.addEventListener("click", () => {
+        // liElement.textContent = "";
+        containerUL.removeChild(liElement);
+    })
+    return delButton;
+}
 
