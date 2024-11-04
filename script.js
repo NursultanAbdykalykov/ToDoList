a4ec363752f8c98e8a6ccf6b22d57b5a0951ef40
Nursultan Abdykalykov <znslt24@gmail.com>
2024-11-04 21:09:23 +0300

    now array is objects

--- script.js
+++ script.js
@@ -4,21 +4,31 @@ const inputBox = document.querySelector(".addInput");
 const buttonAdd = document.querySelector(".add-todo");
 const containerUL = document.querySelector(".todo-list");
 
-const data = ["Сделать дз", "Помыть посуду", "Погулять с собакой"];
+// const data = ["Сделать дз", "Помыть посуду", "Погулять с собакой"];
+const data = [
+    {task: "Сделать дз", isDone: true, id: 1},
+    {task: "Помыть посуду", isDone: false, id: 2},
+    {task: "Погулять с собакой", isDone: false, id: 3},
+];
 
 function renderToDos(array) {
-    for (const item of array) {
+    for (let i = 0; i < array.length; i++) {
         let li = document.createElement('li');
         let span = document.createElement('span');
-        const checkButton = createInputCheckbox(li, span);
+        const checkButton = createInputCheckbox(li, span, array[i]);
         li.appendChild(checkButton);
-        span.textContent = item;
-        const editButton = createEditButton(li, span);
+        span.textContent = array[i].task;
+        const editButton = createEditButton(li, span, array[i]);
         const delButton = createDeleteButton(li, span);
+        if (array[i].isDone === true) {
+            span.style.textDecoration = 'line-through';
+        } else {
+            span.style.textDecoration = 'none';
+        }
         containerUL.appendChild(li);
         li.appendChild(span);
         li.append(editButton, delButton);
-    };
+    }
 }
 
 renderToDos(data);
@@ -41,17 +51,40 @@ function createInput(liElement, spanElement) {
     return inputEdit;
 }
 
-function createEditButton(liElement, spanElement) {
+function createEditButton(liElement, spanElement, todoItem) {
     const editButton = document.createElement("button");
     editButton.classList.add("editButton");
     editButton.textContent = "Редактировать";
+
     editButton.addEventListener("click", () => {
-        const inputEdit = createInput(liElement, spanElement);
+        const inputEdit = document.createElement("input");
+        inputEdit.style.width = "25%";
+        inputEdit.value = spanElement.textContent;
+
+        inputEdit.addEventListener('change', (event) => {
+            liElement.textContent = ""; // Очищаем элемент для обновления
+            spanElement.textContent = event.target.value;
+
+
+            todoItem.task = event.target.value;
+
+
+            const checkButton = createInputCheckbox(liElement, spanElement, todoItem);
+            const editButton = createEditButton(liElement, spanElement, todoItem);
+            const delButton = createDeleteButton(liElement, spanElement);
+
+            liElement.appendChild(checkButton);
+            liElement.appendChild(spanElement);
+            liElement.append(editButton, delButton);
+        });
+
         liElement.textContent = "";
         liElement.appendChild(inputEdit);
     });
+
     return editButton;
 }
+
 
 buttonAdd.addEventListener("click", () => {
     if (inputBox.value === '') {
@@ -65,9 +98,12 @@ buttonAdd.addEventListener("click", () => {
         // containerUL.appendChild(li);
         // li.appendChild(span);
         // li.append(editButton, delButton);
-        data.push(inputBox.value);
-        renderOneToDo(inputBox.value);
+
+        const obj = {task: inputBox.value, isDone: false};
+        data.push(obj);
         console.log(data);
+        renderOneToDo(obj);
+        // console.log(data);
     }
     inputBox.value = '';
 });
@@ -77,26 +113,34 @@ function createDeleteButton(liElement, spanElement) {
     delButton.classList.add("delButton");
     delButton.textContent = "Удалить";
     delButton.addEventListener("click", () => {
-        // liElement.textContent = "";
         containerUL.removeChild(liElement);
     })
     return delButton;
 }
 
-function createInputCheckbox(liElement, spanElement) {
+function createInputCheckbox(liElement, spanElement, todoItem) {
     const checkButton = document.createElement("input");
     checkButton.classList.add("checkbox");
     checkButton.setAttribute('type', 'checkbox');
+
+
+    checkButton.checked = todoItem.isDone;
+
     checkButton.addEventListener("change", () => {
         if (checkButton.checked) {
             spanElement.style.textDecoration = 'line-through';
+            todoItem.isDone = true;
+            console.log(data);
         } else {
             spanElement.style.textDecoration = 'none';
+            todoItem.isDone = false;
+            console.log(data);
         }
+    });
 
-    })
     return checkButton;
 }
+
 
 function addTask() {
     let li = document.createElement('li');
@@ -113,10 +157,15 @@ function addTask() {
 function renderOneToDo(element) {
     let li = document.createElement('li');
     let span = document.createElement('span');
-    const checkButton = createInputCheckbox(li, span);
+    const checkButton = createInputCheckbox(li, span, element);
     li.appendChild(checkButton);
-    span.textContent = element;
-    const editButton = createEditButton(li, span);
+    span.textContent = element.task;
+    if (element.isDone === true) {
+        span.style.textDecoration = 'line-through';
+    } else {
+        span.style.textDecoration = 'none';
+    }
+    const editButton = createEditButton(li, span, element);
     const delButton = createDeleteButton(li, span);
     containerUL.appendChild(li);
     li.appendChild(span);
